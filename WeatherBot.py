34eb81494320d24b2a0527b663e5814a54901064
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-03-22 22:00:05 -0700

    Update WeatherBot.py

--- WeatherBot.py
+++ WeatherBot.py
@@ -1,89 +1,84 @@
+# Talk to me about the weather today!
+# Link: https://py3.codeskulptor.org/#user303_86WogR5aok_0.py
+
 import random
 
 print("Hi there! What's your name?")
 
-name = input("My name is ")
+name = input("My name is...")
 
-print("Nice to meet you " + name + ". I'm WeatherBot! \n")
+print("Nice to meet you " + name + ". I'm WeatherBot!")
 
 life = input("How's life? Good? ")
 
 if(life == "Yes" or life == "yes" or life == "yeet" or life == "Yeet"):
-    print("I'm glad to hear that! My life's been pretty good as well.\n")
+    print("\nI'm glad to hear that! My life's been pretty good as well.")
 else:
-    print("I'm sorry, I wish I could help.")
+    print("\nI'm sorry, I wish I could help.")
     do = input("Would a boop make you feel better? ")
     if(do == "Yes" or do == "yes" or do == "yeet" or do == "Yeet"):
-        print("Boop!")
+        print("\nBoop!")
     else:
-        print("Aw okay. Boops are pretty great though.\n")
+        print("\nAw okay. Boops are pretty great though.\n")
 
 weather = input("So tell me, " + name + ", is it cold outside today? ")
 
 if(weather == "Yes" or weather == "yes" or weather == "yeet" or weather == "Yeet"):
-    temperature = int(input("Brr. What temperature in Farenheit is it? "))
-    if(temperature >= 75):
-        secondweather = input("\nWait a second, that's not cold! Are you sure it's cold?? ")
+    print("\nBrr. What temperature in Farenheit is it? ")
+    temperature = input("Hm?")
+    if(temperature == "idk" or temperature == "IDK" or temperature == "I don't know"):
+        print("\nIt's okay if you don't know. Farenheit is funky that way.")
+    elif(int(temperature) >= 75):
+        print("\nWait a second, that's not cold! ")
+        secondweather = input("Are you sure it's cold?? ")
         if(secondweather == "Yes" or secondweather == "yes" or secondweather == "yeet" or secondweather == "Yeet"):
-            number = random.randint(1, 3)
-            if(number == 1):
-                print("Oooookay then, if you're sure...")
-            elif(number == 2):
-                print("You should probably go put on a jacket then if you think it's cold.")
-            elif(number == 3):
-                print("You might want to check again...?")
+            secondweatheryes = random.choice(["Oooookay then, if you're sure...", "You should probably go put on a jacket then if you think it's cold.", "You might want to check again...?"])
+            print("\n" + secondweatheryes)
         else:
-            print("I'm confused now, but okay, if you say so!")
-    if(temperature <= 75 and temperature >= 30):
-        jacket = input("Wow, that is really cold! Are you wearing a jacket?")
+            print("\nI'm confused now, but okay, if you say so!")
+    elif(int(temperature) <= 75 and int(temperature) >= 30):
+        print("\nWow, that is really cold!")
+        jacket = input("Are you wearing a jacket?")
         if(jacket == "yes" or jacket == "Yes" or jacket == "yeet" or jacket == "Yeet"):
             print("Oof, maybe go put on another one? And several blankets?")
         else:
             print("Maybe you should go put one on then...")
-    if(temperature <= 30):
-        number2 = random.randint(1, 3)
-        if(number2 == 1):
-            print("Oh my gosh, that's so cold!! Don't get hypothermia!")
-        elif(number2 == 2):
-            print("Woah, you better stay inside and keep warm!")
-        elif(number2 == 3):
-            print("Oh jeez, that sounds bad. Drink hot chocolate!")
+    elif(int(temperature) <= 30):
+        freezingtemp = random.choice(["Oh my gosh, that's so cold!! Don't get hypothermia!", "Woah, you better stay inside and keep warm!", "Oh jeez, that sounds bad. Drink hot chocolate!"])
+        print(freezingtemp)
+    
 else:
-    notcold = input("\nSo then is it warm outside? ")
+    print("\nSo then is it warm outside? ")
+    notcold = input("Hm?")
     if(notcold == "Yes" or notcold == "yes" or notcold == "yeet" or notcold == "Yeet"):
-        warmact = input("Warm is a wonderful temperature! Are you going to do anything fun or exciting later? ")
+        print("Warm is a wonderful temperature! ")
+        warmact = input("Are you going to do anything fun or exciting later? ")
         if(warmact == "Yes" or warmact == "yes" or warmact == "yeet" or warmact == "Yeet"):
-            number3 = random.randint(1, 3)
-            if(number3 == 1):
-                print("Well, I hope you have fun!")
-            elif(number3 == 2):
-                print("Lucky! Have a great time!")
-            elif(number3 == 3):
-                print("That's awesome to hear!")
+            warmactyes = random.choice(["Well, I hope you have fun!", "Lucky! Have a great time!", "That's awesome to hear!"])
+            print(warmactyes)
         else:
             print("Aww, I'm sorry. I'm not doing anything fun or exciting either, so I know how you feel!")
     else:
-        notcoldwarm = input("\nSo then I'm assuming it's hot outside. Is it? ")
+        print("\nSo then I'm assuming it's hot outside. ")
+        notcoldwarm = input("Is it? ")
         if(notcoldwarm == "Yes" or notcoldwarm == "yes" or notcoldwarm == "yeet" or notcoldwarm == "Yeet"):
-            hottemp = int(input("How hot? In Farenheit? "))
+            print("\nHow hot? ")
+            hottemp = int(input("In Farenheit? "))
             if(hottemp >= 75):
-                number4 = random.randint(1, 3)
-                if(number4 == 1):
-                    print("Ah, nice! I love hot days!")
-                elif(number4 == 2):
-                    print("Hot days are perfect for beach days! Why not hang out there?")
-                elif(number4 == 3):
-                    print("Better turn up the AC!")
+                hottempans = random.choice(["Ah, nice! I love hot days!", "Hot days are perfect for beach days! Why not hang out there?", "Better turn up the AC!", "Put on sunblock if it's also sunny!"])
             elif(hottemp <= 75 and hottemp >= 30):
-                hotcheck = input("Hey, wait a second! That's not that hot! Are you sure it's hot out?")
+                print("Hey, wait a second! That's not that hot! ")
+                hotcheck = input("Are you sure it's hot out?")
                 if(hotcheck == "Yes" or hotcheck == "yes" or hotcheck == "yeet" or hotcheck == "Yeet"):
                     print("Okay, if you're sure...")
+                else:
+                    print("I knew it! You're just trying to trick me! ;)")
             elif(hottemp <= 30):
                 print("WOAH, that's not hot at all! How are you not freezing yet??")
         else:
-            confuseweather = input("\nYou're beginning to confuse me...so then what is the weather outside? If it's not cold, warm, or hot? ")
-            print("Ah, that makes sense now. I gotchu!")
-            print("Thanks for talking to me! Even if your answers were a little confusing :p")
+            print("\nYou're beginning to confuse me...so then what is the weather outside? ")
+            confuseweather = input("What's the weather if it's not cold, warm, or hot? ")
+            print("Ah, that makes sense now. I gotchu! \n\nThanks for talking to me! \nEven if your answers were a little confusing :p")
 
-print("I hope you have a nice day!")
+print("\nI hope you have a nice day!")
             
