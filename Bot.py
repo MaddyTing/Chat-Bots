e5d7efdcf49924d793d9c682afa7c6fb0cc3b80a
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-04-12 09:43:23 -0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,12 @@
 # Talk to me about the 
+
+
+use in = input("hello!")
+
+if(val.find ("hello")):
+    print("nice to meet you!")
+    day = input("having a good day?")
+    day = day.lower
+    if(day.find("no"):
+       print("aww")
+    
