2a30a3cde5311d85073f92907de0fbadd63085d7
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-04-16 10:19:09 -0700

    Update FredTranslator.py

--- FredTranslator.py
+++ FredTranslator.py
@@ -3,5 +3,7 @@ print("Hello there! What's your name?")
 name = input("My name is...")
 name = name.capitalize()
 
+print("Nice to meet you " + name + ". I'm the one and only FredTranslator.")
+
 print("So you just got a FredTalk. What did he say to you?")
 #list
