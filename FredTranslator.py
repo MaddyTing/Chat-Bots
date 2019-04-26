e62a5b1c539da53016d06d678ed4fedfb89caaf8
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-04-26 10:13:46 -0700

    Update FredTranslator.py

--- FredTranslator.py
+++ FredTranslator.py
@@ -34,16 +34,3 @@ if(fredtalk.find("Y") > -1 or fredtalk.find("y") > -1):
 else:
     print("\nIf you didn't get a FredTalk, then why are you even here??\n...I mean...I am the FredTranslator after all...")
     print("\nAnyway, you should be glad you've been spared...for now...")
-'''
-    print("\n--- " + fredtalk)
-    print("\nWait...did you just say no?? Did I hear you correctly?")
-    qfredtalk = input("Did you say no?")
-    qfredtalk = qfredtalk.capitalize()
-    if(qfredtalk.find("Y") > -1 or qfredtalk.find("y") > -1):
-        print("\n--- " + qfredtalk)
-        print("\nIf you didn't get a FredTalk, then why are you even here??")
-        print("\nYou should be glad you've been spared...for now...")
-    else:
-        print("Well, okay then.")
-        input("So what did Fred say to you?")
-'''
