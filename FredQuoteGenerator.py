94def3bf2bcba548c98a0b9c787c647a7382a938
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-04-26 10:16:06 -0700

    Update FredQuoteGenerator.py

--- FredQuoteGenerator.py
+++ FredQuoteGenerator.py
@@ -1,1 +1,12 @@
+# Short but sweet = searches for one less than 20 characters
+# etc
 
+# Generates a random Fred quote
+
+print("Hey there! What's your name?")
+
+name = int(input("My name is..."))
+
+print("\n--- " + name)
+
+print("Nice to meet you " + name.capitalize() + "! I'm your one and only FredQuoteGenerator.")
